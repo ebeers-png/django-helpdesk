73dd3c83305ee394e1f464aeeff1fb16997c6652
loydbanks <lonwabozaula@gmail.com>
2023-10-27 10:38:52 +0000

    fix: printing variables

--- helpdesk/templatetags/organization_info.py
+++ helpdesk/templatetags/organization_info.py
@@ -70,17 +70,17 @@ def organization_info(user, request):
             print("This is helpdesk staff section")
             logo_path = ""
 
-            if "logos/" not in logo_path:
-                logo_path = return_info["default_org"]['logo'].split("s")[0]+'s'+'/'+return_info["default_org"]['logo'].split("s")[-1]
-            else:
-                logo_path = return_info["default_org"]['logo']
+            # if "logos/" not in logo_path:
+            #     logo_path = return_info["default_org"]['logo'].split("s")[0]+'s'+'/'+return_info["default_org"]['logo'].split("s")[-1]
+            # else:
+            #     logo_path = return_info["default_org"]['logo']
 
-            if settings.USE_S3 is True:
-                return_info['org_logo'] = create_presigned_url(settings.AWS_STORAGE_BUCKET_NAME, f"{settings.MEDIA_ROOT}/{logo_path}")
-            else:
-                return_info['org_logo'] = settings.MEDIA_ROOT + logo_path
+            # if settings.USE_S3 is True:
+            #     return_info['org_logo'] = create_presigned_url(settings.AWS_STORAGE_BUCKET_NAME, f"{settings.MEDIA_ROOT}/{logo_path}")
+            # else:
+            #     return_info['org_logo'] = settings.MEDIA_ROOT + logo_path
             
-            print("print for return_info", return_info['org_logo'], return_info["default_org"].logo)
+            print("print for return_info", return_info["default_org"].logo)
 
         else:
             helpdesk_orgs = get_helpdesk_orgs_for_domain(domain_id)
