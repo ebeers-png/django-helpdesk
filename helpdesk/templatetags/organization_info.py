cc0db6ce299c620347182cd2ab9f2c47333409a5
loydbanks <lonwabozaula@gmail.com>
2023-10-27 11:05:00 +0000

    fix: print logo_path

--- helpdesk/templatetags/organization_info.py
+++ helpdesk/templatetags/organization_info.py
@@ -70,10 +70,12 @@ def organization_info(user, request):
             print("This is helpdesk staff section")
             logo_path = ""
 
-            # if "logos/" not in logo_path:
-            #     logo_path = return_info["default_org"]['logo'].split("s")[0]+'s'+'/'+return_info["default_org"]['logo'].split("s")[-1]
-            # else:
-            #     logo_path = return_info["default_org"]['logo']
+            if "logos/" not in logo_path:
+                logo_path = return_info["default_org"].logo.split("s")[0]+'s'+'/'+return_info["default_org"].logo.split("s")[-1]
+            else:
+                logo_path = return_info["default_org"].logo
+            
+            print("logo path is:", logo_path)
 
             # if settings.USE_S3 is True:
             #     return_info['org_logo'] = create_presigned_url(settings.AWS_STORAGE_BUCKET_NAME, f"{settings.MEDIA_ROOT}/{logo_path}")
